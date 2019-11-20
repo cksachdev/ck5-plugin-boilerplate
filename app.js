8acee8a0b05056a93ba03b530b215a2cbf60b0f9
Nikunj Beladiya <nikunj.beladiya@tarento.com>
2019-11-20 16:10:37 +0530

    Listen for double click event from CK plugin and open add image dialog box

--- app.js
+++ app.js
@@ -12,17 +12,19 @@ import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
 import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
 
+import ClickObserver from '@ckeditor/ckeditor5-engine/src/view/observer/clickobserver';
 
 class InsertImage extends Plugin {
     init() {
         const editor = this.editor;
+        const view = editor.editing.view;
+        const viewDocument = view.document;
 
-        // window.addEventListener("asdfasf", function(editor) {
+        // Somewhere in your plugin's init() callback:
+        view.addObserver( ClickObserver );
 
-        // });
         editor.ui.componentFactory.add('insertImage', locale => {
             const view = new ButtonView(locale);
-
             view.set({
                 label: 'Add Equations',
                 withText: true,
@@ -33,21 +35,43 @@ class InsertImage extends Plugin {
             this.listenTo(view, 'execute', () => {
                 openModel();
             });
-
-            window.addEventListener('setDatatoCK', function(data){
-                editor.model.change( writer => {
-                    const imageElement = writer.createElement( 'image', {
-                        src: data.detail
-                    } );
-                    // Insert the image in the current selection location.
-                    editor.model.insertContent( imageElement, editor.model.document.selection );
-                } );
-            })
-
             return view;
         });
+
+        window.addEventListener('setDatatoCK', function(data){
+            editor.model.change( writer => {
+                const imageElement = writer.createElement( 'image', {
+                    src: data.detail
+                } );
+                // Insert the image in the current selection location.
+                editor.model.insertContent( imageElement, editor.model.document.selection );
+            } );
+        })
+
+        this.listenTo( editor.editing.view.document, 'click', ( evt, data ) => {
+            // Is double click
+            if ( data.domEvent.detail == 2 ) {
+                doubleClickHandler( data.domTarget, data.domEvent );
+                evt.stop();
+            }
+        }, { priority: 'highest' } );
     }
-}
+};
+
+function doubleClickHandler(element, event) {
+    if (element.nodeName.toLowerCase() == 'img') {
+        console.log(element);
+        console.log(event);
+        if (typeof event.stopPropagation != 'undefined') { // old I.E compatibility.
+            event.stopPropagation();
+        } else {
+            event.returnValue = false;
+        }
+        loadDataFromCktoPopup({src: element.src});
+        evt.stop();
+    }
+    
+};
 
 ClassicEditor
     .create(document.querySelector('#editor'), {

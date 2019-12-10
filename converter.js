463f262999429af88502e53c35b456a9512717e6
Nikunj Beladiya <nikunj.beladiya@tarento.com>
2019-12-10 14:59:46 +0530

    Updated ckfinder files

--- converter.js
+++ converter.js
@@ -5,24 +5,27 @@ function replaceWithStreams(path) {
     var fileContent = fs.readFileSync(path, 'utf8');
 
     // replacePath is your match[1]
-    fileContent = fileContent.replace(REGEX, function replacer(matchString, replacePath) {
-        console.log("Match string ",matchString);
-        console.log(replacePath);
-        let replaceItem = matchString.match(/(?<=(['"])\b)(?:(?!\1|\\).|\\.)*(?=\1)/);
-        //let replaceItem = getString(match.replace('")', '').replace('S("', ''));
+     fileContent = fileContent.replace(REGEX, function replacer(matchString, replacePath) {
+        let replaceItem;
+        eval("replaceItem=eval(matchString)");
         console.log('replaceItem', replaceItem)
+        matchString = replaceItem;
         // load and return the replacement file
         return fs.readFileSync(replacePath, 'utf8');
     });
 
     // this will overwrite the original html file, change the path for test
-    //fs.writeFileSync(path, fileContent);
-    
+    fs.writeFileSync(path, fileContent);
 }
 
-function getString(e) {
-    for (var t = "", n = e.charCodeAt(0), i = 1; i < e.length; ++i)
-        t += String.fromCharCode(e.charCodeAt(i) ^ i + n & 127);
+function S(e) {
+    console.log(typeof e);
+    // let str2 = wtf8.decode(e.toString());
+    // console.log(str2)
+    let str2;
+    str2=e;
+    for (var t = "", n = str2.charCodeAt(0), i = 1; i < str2.length; ++i)
+        t += String.fromCharCode(str2.charCodeAt(i) ^ i + n & 127);
     return t
 }
 

71c7e85d095f7258258d994f17ad73ef3f9cbd82
Chetan Sachdev <cksachdev@gmail.com>
2020-01-28 15:23:52 +0530

    Added reference link to the issue describing about double click behaviour

--- plugin/doubleClickObserver.js
+++ plugin/doubleClickObserver.js
@@ -1,11 +1,12 @@
 import DomEventObserver from '@ckeditor/ckeditor5-engine/src/view/observer/domeventobserver';
 
+//Reference: https://github.com/ckeditor/ckeditor5/issues/2077
 export default class DoubleClickObserver extends DomEventObserver {
-	constructor( view ) {
-		super( view );
-		this.domEventType = 'dblclick';
-	}
-	onDomEvent( domEvent ) {
-		this.fire( domEvent.type, domEvent );
-	}
+    constructor(view) {
+        super(view);
+        this.domEventType = 'dblclick';
+    }
+    onDomEvent(domEvent) {
+        this.fire(domEvent.type, domEvent);
+    }
 }

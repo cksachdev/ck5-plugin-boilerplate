1a969b3903c83ac06ecf5f36a30c6728335c2346
Nikunj Beladiya <nikunj.beladiya@tarento.com>
2020-01-30 18:27:09 +0530

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+# deploy.sh
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+echo Building. this may take a minute...
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'example.com' > CNAME
+
+echo Deploying..
+git init
+git add -A
+git commit -m 'deploy'
+
+# deploy
+git push -f https://github.com/cksachdev/ck5-plugin-boilerplate.git master:gh-pages
+
+cd -
